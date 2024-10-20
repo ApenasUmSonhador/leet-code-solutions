4a7a5b0be5a9ccf401e7c8887a81844d469dd09e
Arthur Nunes <arthurvininunes@gmail.com>
2024-10-20 13:56:36 -0300

    20/10/2024 - Day 1: By far this solution is mine favorite

--- Arrays & Hashing/15-3sum.py
+++ Arrays & Hashing/15-3sum.py
@@ -1,23 +1,19 @@
 class Solution(object):
     def threeSum(self, nums):
-        ans,num_list = [], nums
-        while len(num_list) > 2:
-            n = num_list.pop(0)
-            target = -n
-            l = self.twoSum(num_list, target)
-            if l is not None:
-                for ll in l:
-                    ll.append(n)
-                for ll in l:
-                    ll.sort()
-                    if ll not in ans:
-                        ans.append(ll)
-        return ans
-    def twoSum(self, num_list, target):
-        num_dict, ans = {}, []
-        for i, num in enumerate(num_list):
-            diff = target - num
+        ans = set()
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                # because we have already checked the previous number
+                continue
+            self.twoSum(nums, i, ans)
+        return [list(triplet) for triplet in ans]
+
+    def twoSum(self, nums, i, ans):
+        num_dict = {}
+        target = -nums[i]
+        for j in range(i+1, len(nums)):
+            diff = target - nums[j]
             if diff in num_dict:
-                ans.append([diff, num])
-            num_dict[num] = i
-        return ans
+                ans.add((nums[i], diff, nums[j]))
+            num_dict[nums[j]] = j
