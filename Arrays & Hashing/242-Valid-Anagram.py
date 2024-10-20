4a41b38d473be3f8f9d585e1742d22c2c161c24e
Arthur Nunes <arthurvininunes@gmail.com>
2024-10-20 12:28:08 -0300

    20/10/2024 - Day 1: Solution using ord and left-right

--- Arrays & Hashing/242-Valid-Anagram.py
+++ Arrays & Hashing/242-Valid-Anagram.py
@@ -1,15 +1,17 @@
 class Solution(object):
-    def isAnagram(self, s, t):
-        original = dict()
-        test = dict()
-        for i in s:
-            if i in original:
-                original[i] += 1
-            else:
-                original[i] = 0
-        for i in t:
-            if i in test:
-                test[i] += 1
-            else:
-                test[i] = 0
-        return test == original
+    def isPalindrome(self, s):
+        l, r = 0, len(s)-1
+        while l <= r:
+            while l < r and not self.isAlnum(s[l]):
+                l += 1
+            while r > l and not self.isAlnum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l+1, r-1
+        return True
+        
+    def isAlnum(self, a):
+        return (ord('A') <= ord(a) <= ord('Z') or
+                ord('a') <= ord(a) <= ord('z') or
+                ord('0') <= ord(a) <= ord('9'))
