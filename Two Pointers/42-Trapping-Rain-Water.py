c31e8e0914f4819431d8ea182579b55bb6eb9756
Arthur Nunes <arthurvininunes@gmail.com>
2024-11-11 00:08:02 -0300

    correção

--- Two Pointers/42-Trapping-Rain-Water.py
+++ Two Pointers/42-Trapping-Rain-Water.py
@@ -5,10 +5,7 @@ class Solution(object):
         ml[0], mr[n-1] = height[0], height[n-1]
         for i in range(1, n):
             ml[i] = max(height[i], ml[i-1])
-        i = n-2
-        while i >= 0:
-            mr[i] = max(height[i], mr[i+1])
-            i -= 1
+            mr[n-i-1] = max(height[n-i-1], mr[n-i-2])
         for i in range(n):
             a += min(ml[i], mr[i]) - height[i]
         return a
