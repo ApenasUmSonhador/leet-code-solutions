ce8536891fbc32530c838fba07aebcb9fc7d1a3c
Arthur Nunes <arthurvininunes@gmail.com>
2025-02-28 17:22:03 -0300

    Day 8: 28/02/2025: Much better solution

--- Arrays & Hashing/49-Group-Anagrams.py
+++ Arrays & Hashing/49-Group-Anagrams.py
@@ -1,25 +1,10 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        palindromes = {}
-        solution = []
-        for new in strs:
-            new_dict = {}
-            for letter in new:
-                if letter in new_dict:
-                    new_dict[letter] += 1
-                else:
-                    new_dict[letter] = 1
-            
-            i, stop = 0, False
-            while i < len(solution) and not stop:
-                if palindromes[solution[i][0]] == new_dict:
-                    stop = True
-                else:
-                    i += 1
-            if stop:
-                solution[i].append(new)
+        anagrams = {}
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key in anagrams:
+                anagrams[key].append(s)
             else:
-                palindromes[new] = new_dict
-                solution.append([new])
-        return solution
-        
+                anagrams[key] = [s]
+        return list(anagrams.values())
